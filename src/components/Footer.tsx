aee9aea510b8b16455fcd799aab940575bd3b5e8
PraditaAdiantama <mpaproject307@gmail.com>
2025-01-02 22:06:06 +0700

    init footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="w-full max-md:px-5 shadow-xl bg-white rounded-xl hover:shadow-2xl mt-5 py-5">
-      <p className="text-center font-medium">
+    <div className="w-full max-md:px-5 shadow-xl bg-white dark:bg-slate-800 rounded-xl hover:shadow-2xl mt-5 py-5">
+      <p className="text-center font-medium dark:text-white">
         © 2024 Muhamad Pradita Adiantama. All rights reserved.
       </p>
     </div>
