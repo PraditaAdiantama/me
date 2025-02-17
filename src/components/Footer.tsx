d3ce292af0b1b53fe1a6d431969248dec85cce87
PraditaAdiantama <mpaproject307@gmail.com>
2025-02-17 20:49:25 +0700

    init dynamic year 'hehe'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
+	const year = new Date().getFullYear()
   return (
     <div className="w-full max-md:px-5 shadow-xl bg-white dark:bg-slate-800 rounded-xl hover:shadow-2xl mt-5 py-5">
       <p className="text-center font-medium dark:text-white">
-        © 2024 Muhamad Pradita Adiantama. All rights reserved.
+        © {year} Muhamad Pradita Adiantama. All rights reserved.
       </p>
     </div>
   );
